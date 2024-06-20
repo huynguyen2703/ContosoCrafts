862727efefa00d1e11d7a80912a3577743089816
vlqv9210 <vvuong@seattleu.edu>
2024-06-20 02:56:03 -0700

    Add project files.

---
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ContosoCrafts.WebSite.Model;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Website.Controllers
+{   // route is determined by class name with suffix Controller
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        JsonFileProductService productServiceObject;
+        public ProductsController(JsonFileProductService productService) 
+        {
+            productServiceObject = productService;
+        }
+
+        public JsonFileProductService ProductService => productServiceObject;
+
+        [HttpGet]
+        public IEnumerable<Product> Get() 
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, [FromQuery]int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
+
+    }
+}
